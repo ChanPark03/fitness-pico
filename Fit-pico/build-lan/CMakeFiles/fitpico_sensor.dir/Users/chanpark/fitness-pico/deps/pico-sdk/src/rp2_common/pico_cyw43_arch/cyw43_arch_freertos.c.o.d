6751891f6dd8d807ba6d2331e234d509977682c1
ChanPark03 <@parkchan0302@gmail.com>
2026-04-02 15:53:55 +0900

    문서정리

--- Fit-pico/build-lan/CMakeFiles/fitpico_sensor.dir/Users/chanpark/fitness-pico/deps/pico-sdk/src/rp2_common/pico_cyw43_arch/cyw43_arch_freertos.c.o.d
+++
@@ -1,2 +0,0 @@
-CMakeFiles/fitpico_sensor.dir/Users/chanpark/fitness-pico/deps/pico-sdk/src/rp2_common/pico_cyw43_arch/cyw43_arch_freertos.c.o: \
- /Users/chanpark/fitness-pico/deps/pico-sdk/src/rp2_common/pico_cyw43_arch/cyw43_arch_freertos.c
